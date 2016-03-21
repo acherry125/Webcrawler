0c319bb00667558ddb222ad3431bc5a2d4bde000
acherry125 <acherry125@gmail.com>
2016-03-21 16:31:38 -0400

    LOGIN SUCCESFUL
    must generalize code in get_cookies and parse_links because they're
    pretty much the same thing with a few little tweaks

--- webcrawler.py
+++ webcrawler.py
@@ -144,8 +144,10 @@ print ("HTTP/1.0 200 OK")
 '\r\n')"""
 
 # -------------------------------------------------------------------------------------------------------------
-# find all links to go through
-def parse_for_links(html):
+
+
+# find all hyperlinks in an html document
+def parse_links(html):
     last = len(html)
     links = []
     # start from the beginning
@@ -161,91 +163,99 @@ def parse_for_links(html):
         links.append(html[first_letter: end_quote])
     return links
 
-
-def request_page(link, cookies='', type='GET', body='',):
+# requests a page/resource
+def request_page(link, cookies=None, type='GET', body='', ):
+    if cookies is None:
+        cookies = []
     global sock
     global sock_addr
+    # is there a body
+    if body:
+        content_length = 'Content-Length: {}\r\n'.format(len(body))
+    else:
+        content_length = ''
     # display as string 'cookie_name:value; cookie_name:value'
     if cookies:
-        cookie_value = '; '.join(cookies)
+        cookie_value = 'Cookie: {}\r\n'.format('; '.join(cookies))
     else:
         cookie_value = ''
     request = ('{} {} HTTP/1.1\r\n'
                 'Host: fring.ccs.neu.edu\r\n'
                 'Connection: keep-alive\r\n'
-                'Cookie:{}\r\n'
+                '{}'
+                '{}'
                 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n'
-                'Accept-Language: en-US,en;q=0.8)\r\n\r\n{}').format(type, link, cookie_value, body)
-    
+                'Accept-Language: en-US,en;q=0.8)\r\n\r\n'
+                '{}').format(type, link, content_length, cookie_value, body)
+    print request
     sock.sendto(request, sock_addr)
     # sends headers first
     server_msg = sock.recvfrom(100000000)
     header = server_msg[0]
+
     # sends body next
     server_msg2 = sock.recvfrom(100000000)
     msg_body = server_msg2[0]
+
     return (header, msg_body)
 
+# gets the cookies in a response header
+# TODO extrapolare common code between get_cookies and parse_links
 def get_cookies(header):
     # yes this is lazy, use while loop like in parse_for_links to get all cookies TODO
     cookies = []
-    # find cookie 1
-    cookie_loc = header.find('Set-Cookie:')
-    end_cookie = header.find(';', cookie_loc, len(header))
-    cookie1 = header[cookie_loc + 12: end_cookie]
-    cookies.append(cookie1)
-    # now find cookie two
-    cookie_loc2 = header.find('Set-Cookie:', cookie_loc + 1, len(header))
-    end_cookie2 = header.find(';', cookie_loc2, len(header))
-    cookie2 = header[cookie_loc2 + 12:end_cookie2]
-    cookies.append(cookie2)
+    cookie_loc = 0
+    while True:
+        # find cookie 1
+        cookie_loc = header.find('Set-Cookie:', cookie_loc + 1, len(header))
+        if cookie_loc == -1:
+            break
+        end_cookie = header.find(';', cookie_loc, len(header))
+        if end_cookie == -1:
+            raise ValueError("the http is formatted wrong")
+        cookie = header[cookie_loc + 12: end_cookie]
+        cookies.append(cookie)
     return cookies
 
+# connects the socket to the server
+def connect_to_server(server='fring.ccs.neu.edu', port=80):
+    global addrinfo
+    global sock_addr
+    global socket_info
+    global sock
+    addrinfo = socket.getaddrinfo(server, port)[0]
+    sock_addr = addrinfo[-1]
 
+    socket_info = addrinfo[:3]
 
-addrinfo = socket.getaddrinfo('fring.ccs.neu.edu', 80)[0]
+    sock = socket.socket(*socket_info)
 
-sock_addr = addrinfo[-1]
+    sock.connect(sock_addr)
 
-socket_info = addrinfo[:3]
+# initial connection
+connect_to_server()
 
-sock = socket.socket(*socket_info)
-
-sock.connect(sock_addr)
-
-
-login_req = '/accounts/login/?next=/fakebook/'
+# the login page path
 login_post = '/accounts/login/'
+# get the initial login page
+(header, msg_body) = request_page(login_post)
 
-
-
-# operation resource_path HTTP/1.1\r\nHost:hostname
-#sock.sendto(login_req, sock_addr)
-(header, msg_body) = request_page(login_req)
-
-#print header
-#print msg_body
-
-
-# for some reason the cookie is given to us as csrftoken
-# but we need to give it back as 'csrfmiddlewaretoken'
-
-# request body to send back
 # username=<username_here>&password=<password_here>&csrfmiddlewaretoken=<csrftoken_here>&next=%2Ffakebook%2F
 cookies = get_cookies(header)
 # assume that cookies[0] is csrftoken
 csrf = cookies[0].split('=')
+# dont merge these
 csrf_val = csrf[1]
-# maybe useful in future
-'''# join together
-cookies = '&'.join(cookies)
-# replace csrftoken with the correct name
-equals_loc = cookies.find('csrftoken=')
-cookies = cookies[: equals_loc] + 'csrfmiddlewaretoken=' + cookies[equals_loc + 10:]'''
 
-login_data = 'username=001783626&password=8XOD2QE4&csrfmiddlewaretoken={}&next=%2Ffakebook%2F'.format(csrf_val)
+login_data = 'username=001783626&password=8XOD2QE4&csrfmiddlewaretoken={}&next=/'.format(csrf_val)
 
-(y, z) = request_page(login_post, cookies, 'POST', login_data)
+(login_redirect_header, login_ignore) = request_page(login_post, cookies, 'POST', login_data)
 
-print y
-print z
+# get the new session id
+session_id = get_cookies(login_redirect_header)
+# have to reset the connection for some reason
+connect_to_server()
+(succes_header, fakebook_home) = request_page('/fakebook/', session_id)
+#---------------------------------------------------------------------------------
+# WE ARE LOGGED IN
+print parse_links(fakebook_home)
